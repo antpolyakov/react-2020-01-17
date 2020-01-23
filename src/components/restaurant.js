3e8a2503e9273f586ece471c80376b5bf15e68e5
Ant Polyakov <ant@polyakov.ru>
2020-01-23 16:39:52 +0300

    implement "average rating" calculation for the Restaurant

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import Menu from './menu'
 import Reviews from './reviews'
 
+const RATING_PRECISION = 0.1
+
 function Restaurant(props) {
+  const avgRating = useMemo(() => {
+    return (
+      Math.round(
+        props.reviews.length &&
+          props.reviews.reduce((sum, review) => sum + review.rating, 0) /
+            props.reviews.length /
+            RATING_PRECISION
+      ) * RATING_PRECISION
+    )
+  }, [props.reviews])
   return (
     <div>
       <h1>{props.name}</h1>
+      <div>
+        Average rating: <b>{avgRating}</b> ({props.reviews.length} reviews)
+      </div>
       <hr />
       <h2>Menu</h2>
       <Menu items={props.menu} />
