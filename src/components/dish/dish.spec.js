634e92a82a174fe3da6745673e338e4397410322
Ant Polyakov <ant@polyakov.ru>
2020-01-27 23:43:50 +0300

    test for Dish cart amount decrease

--- src/components/dish/dish.spec.js
+++ src/components/dish/dish.spec.js
@@ -21,4 +21,37 @@ describe('Dish', function() {
 
     expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('4')
   })
+
+  it('should decrease cart amount when click on minus button', function() {
+    const wrapper = mount(<Dish dish={dishMock} />)
+
+    wrapper
+      .find('button[data-automation-id="INCREASE"]')
+      .simulate('click')
+      .simulate('click')
+      .simulate('click')
+    wrapper
+      .find('button[data-automation-id="DECREASE"]')
+      .simulate('click')
+      .simulate('click')
+      .simulate('click')
+    expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('0')
+  })
+
+  it('should NOT decrease cart amount when amount is zero', function() {
+    const wrapper = mount(<Dish dish={dishMock} />)
+
+    wrapper
+      .find('button[data-automation-id="INCREASE"]')
+      .simulate('click')
+      .simulate('click')
+      .simulate('click')
+    wrapper
+      .find('button[data-automation-id="DECREASE"]')
+      .simulate('click')
+      .simulate('click')
+      .simulate('click')
+      .simulate('click')
+    expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('0')
+  })
 })
