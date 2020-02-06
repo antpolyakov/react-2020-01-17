75f90fef84d3164564130eb35deaa39aca8eece5
Ant Polyakov <ant@polyakov.ru>
2020-02-06 19:05:42 +0300

    implement Reviews tests, 'data-automation-id'

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -6,7 +6,7 @@ import {Col, Row} from 'antd'
 function Reviews({reviews}) {
   return (
     <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
-      <Col xs={24} md={16}>
+      <Col xs={24} md={16} data-automation-id="REVIEWS_CONTAINER">
         {reviews.map(review => (
           <Review review={review} key={review.id} />
         ))}
