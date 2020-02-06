65fa05e89f04aba03525d6cae727909520e6b528
Ant Polyakov <ant@polyakov.ru>
2020-02-06 18:42:46 +0300

    enhanced Dish test suite

--- src/components/dish/dish.spec.js
+++ src/components/dish/dish.spec.js
@@ -6,52 +6,48 @@ import {restaurants} from '../../fixtures'
 
 const dishMock = restaurants[0].menu[0]
 
+const CLICKS_COUNT = 3
+
 Enzyme.configure({adapter: new Adapter()})
 
 describe('Dish', function() {
-  it('should increase cart amount when click on plus button', function() {
+  const nodes = {
+    increaseButton: null,
+    decreaseButton: null,
+    amountNode: null,
+  }
+
+  beforeEach(() => {
     const wrapper = mount(<Dish dish={dishMock} />)
+    nodes.increaseButton = wrapper.find('button[data-automation-id="INCREASE"]')
+    nodes.decreaseButton = wrapper.find('button[data-automation-id="DECREASE"]')
+    nodes.amountNode = wrapper.find('[data-automation-id="AMOUNT"]')
+  })
 
-    wrapper
-      .find('button[data-automation-id="INCREASE"]')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-
-    expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('4')
+  it('should increase cart amount when click on plus button', function() {
+    const {increaseButton, amountNode} = nodes
+    _repeat(CLICKS_COUNT, () => increaseButton.simulate('click'))
+    expect(amountNode.text()).toBe(CLICKS_COUNT.toString())
   })
 
   it('should decrease cart amount when click on minus button', function() {
-    const wrapper = mount(<Dish dish={dishMock} />)
-
-    wrapper
-      .find('button[data-automation-id="INCREASE"]')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-    wrapper
-      .find('button[data-automation-id="DECREASE"]')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-    expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('0')
+    const {increaseButton, decreaseButton, amountNode} = nodes
+    _repeat(CLICKS_COUNT, () => increaseButton.simulate('click'))
+    _repeat(CLICKS_COUNT, () => decreaseButton.simulate('click'))
+    expect(amountNode.text()).toBe('0')
   })
 
   it('should NOT decrease cart amount when amount is zero', function() {
-    const wrapper = mount(<Dish dish={dishMock} />)
-
-    wrapper
-      .find('button[data-automation-id="INCREASE"]')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-    wrapper
-      .find('button[data-automation-id="DECREASE"]')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-      .simulate('click')
-    expect(wrapper.find('[data-automation-id="AMOUNT"]').text()).toBe('0')
+    const {increaseButton, decreaseButton, amountNode} = nodes
+    _repeat(CLICKS_COUNT, () => increaseButton.simulate('click'))
+    _repeat(CLICKS_COUNT + 1, () => decreaseButton.simulate('click'))
+    expect(amountNode.text()).toBe('0')
   })
 })
+
+// TODO replace by _.times()
+const _repeat = (count, action) => {
+  for (let c = 0; c < count; c++) {
+    action()
+  }
+}
