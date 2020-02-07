c7871c7e5bab31e94623b85761f1ad7c8d67b081
Ant Polyakov <ant@polyakov.ru>
2020-02-07 12:24:11 +0300

    fix endless recursion

--- src/store/middlewares/logging.js
+++ src/store/middlewares/logging.js
@@ -1,7 +1,6 @@
 export const logging = store => next => action => {
   console.log('before', store.getState())
   console.log('action', action)
-  store.dispatch({type: ''})
   next(action)
   console.log('after', store.getState())
 }
