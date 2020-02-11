b5dea6bad0ea5a186f5712991a6f37c64b1893bf
Ant Polyakov <ant@polyakov.ru>
2020-02-11 18:19:37 +0300

    enhance dishes loading progress display

--- src/components/dishes/dishes.js
+++ src/components/dishes/dishes.js
@@ -4,22 +4,30 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {fetchDishes} from '../../store/action-creators'
 import {selectDishes} from '../../store/selectors'
+import {Spin} from 'antd'
 
 class Dishes extends Component {
   componentDidMount() {
     this.props.fetchDishes && this.props.fetchDishes()
   }
 
+  renderDishesList = menu => {
+    return menu.map(dishId => <Dish key={dishId} id={dishId} />)
+  }
+
+  renderLoadProgress = () => {
+    return (
+      <h3>
+        <Spin /> loading dishes...
+      </h3>
+    )
+  }
+
   render() {
     const {menu, dishesLoaded} = this.props
-    if (!dishesLoaded) {
-      return <h1>Loading dishes...</h1>
-    }
     return (
       <div>
-        {menu.map(dishId => (
-          <Dish key={dishId} id={dishId} />
-        ))}
+        {dishesLoaded ? this.renderDishesList(menu) : this.renderLoadProgress()}
       </div>
     )
   }
