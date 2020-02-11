f26184c8d83a284ba407ae810e73964e462c155b
Ant Polyakov <ant@polyakov.ru>
2020-02-11 18:16:57 +0300

    track and display on-app-start loading progress (restaurants, users)

--- src/components/restaurants/restaurants.js
+++ src/components/restaurants/restaurants.js
@@ -1,51 +1,92 @@
-import React, {useCallback, useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import Restaurant from '../restaurant'
 import RestaurantsNavigation from '../restaurants-navigation'
 import {connect} from 'react-redux'
-import {selectRestaurants} from '../../store/selectors'
+import {selectRestaurants, selectUserList} from '../../store/selectors'
 import {fetchRestaurants, fetchUsers} from '../../store/action-creators'
+import {Progress} from 'antd'
+
+const defaultLoadProgressState = {
+  restaurants: 'рестораны',
+  users: 'пользователи',
+  //dishes: 'блюда'
+}
 
 function Restaurants(props) {
   const [currentId, setCurrentId] = useState(null)
-  const {fetchRestaurants, fetchUsers} = props
+  const {restaurants, users, fetchRestaurants, fetchUsers} = props
 
   useEffect(() => {
     fetchRestaurants && fetchRestaurants()
     fetchUsers && fetchUsers()
+    // TODO load dishes right here
+    //fetchDishes && fetchDishes()
   }, [fetchRestaurants, fetchUsers])
 
-  const restaurant = props.restaurants.find(
-    restaurant => restaurant.id === currentId
-  )
+  useEffect(() => {
+    !currentId && restaurants.length && setCurrentId(restaurants[0].id)
+  }, [restaurants, currentId])
+
   const handleRestaurantChange = useCallback(id => setCurrentId(id), [
     setCurrentId,
   ])
 
-  useEffect(() => {
-    if (props.restaurants.length === 0) {
-      return
-    }
-    setCurrentId(currentId || props.restaurants[0].id)
-  }, [props.restaurants, currentId])
+  const [loadProgress, setLoadProgress] = useState(defaultLoadProgressState)
 
-  if (props.restaurants.length === 0 || !currentId) {
-    return <h1>Loading...</h1>
+  if (loadProgress.restaurants && restaurants.length > 0) {
+    setLoadProgress({
+      ...loadProgress,
+      restaurants: false,
+    })
+  }
+  if (loadProgress.users && users) {
+    setLoadProgress({
+      ...loadProgress,
+      users: false,
+    })
+  }
+  // TODO loadProgress.dishes && dishes
+
+  const [loadProgressList, loadProgressPercent] = useMemo(() => {
+    const list = Object.values(loadProgress)
+    const inProgressList = list.filter(item => item)
+    // TODO calculate 'loadProgressPercent'
+    return [inProgressList, 100]
+  }, [loadProgress])
+
+  const restaurant = useMemo(
+    () => restaurants.find(restaurant => restaurant.id === currentId),
+    [restaurants, currentId]
+  )
+
+  if (loadProgressList.length) {
+    return (
+      <div>
+        <h3>Загрузка</h3>
+        <Progress
+          percent={loadProgressPercent}
+          showInfo={false}
+          status="active"
+        />
+        <div>{loadProgressList.join(', ')}...</div>
+      </div>
+    )
   }
 
   return (
     <div data-automation-id="RESTAURANTS">
-      <input type={'text'} />
       <RestaurantsNavigation
-        restaurants={props.restaurants}
+        restaurants={restaurants}
         onRestaurantChange={handleRestaurantChange}
       />
-      <Restaurant restaurant={restaurant} />
+      {restaurant && <Restaurant restaurant={restaurant} />}
     </div>
   )
 }
 
 const mapStateToProps = state => ({
   restaurants: selectRestaurants(state),
+  users: selectUserList(state),
 })
 
 const mapDispatchToProps = {
