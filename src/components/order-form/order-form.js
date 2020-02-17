d872359246c1c05234913c0aa9c140711494a27b
Ant Polyakov <ant@polyakov.ru>
2020-02-18 00:52:06 +0300

    translate OrderForm and OrderComplete

--- src/components/order-form/order-form.js
+++ src/components/order-form/order-form.js
@@ -3,27 +3,28 @@ import {Input, Button, Form} from 'antd'
 import {connect} from 'react-redux'
 import {sendOrder} from '../../store/action-creators'
 import {Consumer} from '../../contexts/user'
+import I18nContext from '../../contexts/i18n'
 
 class OrderForm extends Component {
   state = {
     userName: '',
   }
 
-  render() {
+  renderForm(__) {
     return (
       <Form
         layout={'inline'}
         style={{padding: '24px'}}
         onSubmit={this.handleSubmit}
       >
-        <h1 ref={this.setRefForSomeHTMLElement}>{'Form'}</h1>
+        <h1 ref={this.setRefForSomeHTMLElement}>{__('Form')}</h1>
         <Form.Item>
           <Consumer>
             {({name, handleUserChange}) => {
               return (
                 <Input
                   ref={this.setInput}
-                  placeholder={'User name'}
+                  placeholder={__('User name')}
                   value={name}
                   onChange={event => {
                     handleUserChange({
@@ -39,10 +40,18 @@ class OrderForm extends Component {
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit">
-            {'Send order'}
+            {__('Send order')}
           </Button>
         </Form.Item>
       </Form>
+    )
+  }
+
+  render() {
+    return (
+      <I18nContext.Consumer>
+        {({translate}) => this.renderForm(translate)}
+      </I18nContext.Consumer>
     )
   }
 
--- src/routes/order-complete.js
+++ src/routes/order-complete.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Consumer as UserConsumer} from '../contexts/user'
+import I18nContext from '../contexts/i18n'
 
 function OrderComplete() {
+  const {translate: __} = useContext(I18nContext)
+
   return (
     <h1
       style={{
@@ -9,7 +12,7 @@ function OrderComplete() {
         padding: '128px 0',
       }}
     >
-      <UserConsumer>{user => 'Thanks, ' + user.name}</UserConsumer>
+      <UserConsumer>{user => `${__('Thanks')}, ${user.name}`}</UserConsumer>
       <span
         role={'img'}
         aria-label={'cook'}
